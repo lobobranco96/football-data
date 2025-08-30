b5d5665c13890ffdd34b49d85267a6e9c65ca4a3
lobobranco96 <rntt2511@gmail.com>
2025-08-30 12:27:54 +0000

    fix methods

--- mnt/src/python/extract.py
+++ mnt/src/python/extract.py
@@ -36,14 +36,16 @@ class ExtractWikiData:
             4. Extract column headers.
             5. Extract each row of the table.
             6. Combine columns and rows into a list of dictionaries.
+            7. collect team url necessary to the next task
 
         Returns:
             List[Dict[str, str]]: A list where each element is a dictionary representing a team,
             with column names as keys and cell values as values.
+            List[str]: A list of URLs for each team's page.
         """
         try:
-            logging.info(f"Accessing URL: {self.url}")
-            self.driver.get(self.url)
+            logging.info(f"Accessing URL: {url}")
+            self.driver.get(url)
         except Exception as e:
             logging.error(f"Error accessing the URL: {e}")
             raise
@@ -65,7 +67,7 @@ class ExtractWikiData:
 
         # Extract all rows
         rows = list()
-        links = list() # store url pages from players team
+        urls = list() # store url pages from players team
         for tr in wiki_data.find_all("tr")[1:]:
             teams = tr.find_all("td")
             if teams:
@@ -76,43 +78,17 @@ class ExtractWikiData:
                         team_name = td.get_text(" ", strip=True).replace("\xa0", "")
                         href = a_tag.get("href") if a_tag else None
                         row.append(team_name)
-                        links.append("https://pt.wikipedia.org" + href if href else None)
+                        urls.append("https://pt.wikipedia.org" + href if href else None)
                     else:
                         row.append(td.get_text(" ", strip=True).replace("\xa0", ""))
                 rows.append(row)
 
-        # Combine columns and rows into a list of dictionaries
-        return [dict(zip(columns, row)) for row in rows], links
+        # Combine columns and rows into a list of dictionaries, a list of url
+        return [dict(zip(columns, row)) for row in rows], urls
 
-    def collect_full_players_data(self, team_url_list):
-      """
-      Itera sobre todas as URLs de times e junta todos os jogadores em uma lista de dicionários.
-
-      Args:
-          lista_times (List[str]): Lista de URLs das páginas de cada time.
-
-      Returns:
-          List[Dict]: Lista com todos os jogadores de todos os times.
-      """
-      players = []
-
-      for team_url in team_url_list:
-          try:
-              players_team = self.brasileirao_players(team_url)
-              if players_team:
-                  players.extend(players_team)
-                  logging.info(f"Extraídos {len(players_team)} jogadores de {team_url}")
-              else:
-                  logging.warning(f"{team_url} - Falhou: retornou lista vazia")
-          except Exception as e:
-              logging.error(f"Erro ao extrair jogadores de {team_url}: {e}")
-
-
-      return players
-
-    def brasileirao_team_players(self, team_url):
+    def brasileirao_players(self, team_url):
         try:
-            logging.info(f"Accessing URL: {self.url}")
+            logging.info(f"Accessing URL: {team_url}")
             self.driver.get(team_url)
         except Exception as e:
             logging.error(f"Error accessing the URL: {e}")
@@ -153,5 +129,30 @@ class ExtractWikiData:
 
         return players_list
 
+    def collect_full_players_data(self, team_url_list):
+      """
+      Itera sobre todas as URLs de times e junta todos os jogadores em uma lista de dicionários.
 
+      Args:
+          lista_times (List[str]): Lista de URLs das páginas de cada time.
+
+      Returns:
+          List[Dict]: Lista com todos os jogadores de todos os times.
+      """
+      players = []
+
+      for team_url in team_url_list:
+          try:
+              print(f"URL carregada: {team_url}")
+              players_team = self.brasileirao_players(team_url)
+              if players_team:
+                  players.extend(players_team)
+                  logging.info(f"Extraídos {len(players_team)} jogadores de {team_url}")
+              else:
+                  logging.warning(f"{team_url} - Falhou: retornou lista vazia")
+          except Exception as e:
+              logging.error(f"Erro ao extrair jogadores de {team_url}: {e}")
+
+
+      return players
 
